51b9c23ea2a49afb48c6b8a9df93279211786c58
unknown <raquelresende_@outlook.com>
2023-02-18 19:31:19 -0300

    python -elif

---
+++ ex006.1.py
@@ -0,0 +1,11 @@
+
+nota = int(input("Digite a nota:"))
+
+if nota <= 3:
+    print("Reprovado")
+else:
+   if nota <=7:
+      print("Recuperação")
+
+   else:
+       print("Aprovado")
---
+++ ex006.2.py
@@ -0,0 +1,10 @@
+
+nota = int(input("Digite a nota:"))
+if nota <= 3:
+    print("Resprovado")
+
+elif nota <= 7:
+    print("Rescuperação")
+
+else:
+    print("Aprovado")
